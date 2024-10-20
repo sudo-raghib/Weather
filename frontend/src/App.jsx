b6208875a488381c21d1fa8546ed198c1dd0db9a
Raghib Haider <95671823+sudo-raghib@users.noreply.github.com>
2024-10-21 00:07:17 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CurrentWeather from "./pages/CurrentWeather";
 import WeatherSummary from "./pages/WeatherSummary";
+import { Header } from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<CurrentWeather />} />
-        <Route path="/summary" element={<WeatherSummary />} />
-      </Routes>
+      <div>
+        <Header />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<CurrentWeather />} />
+            <Route path="/summary" element={<WeatherSummary />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
