de7fa8e53d0fb192856066c7af976f505bb3917a
Raghib Haider <95671823+sudo-raghib@users.noreply.github.com>
2024-10-21 00:08:00 +0530

    final

--- backend/routes/weatherRoutes.js
+++ backend/routes/weatherRoutes.js
@@ -12,6 +12,10 @@ router.get("/current/:city", async (req, res) => {
   try {
     const { city } = req.params;
     const currentWeather = await fetchWeatherData({ city });
+
+    // Process the weather data to ensure the summary is up-to-date
+    await processWeatherData({ city });
+
     res.status(200).json(currentWeather);
   } catch (error) {
     res
@@ -20,24 +24,47 @@ router.get("/current/:city", async (req, res) => {
   }
 });
 
-router.get("/summary/:city", async (req, res) => {
+// router.get("/summary/:city", async (req, res) => {
+//   try {
+//     const { city } = req.params;
+//     const today = new Date();
+//     today.setHours(0, 0, 0, 0);
+
+//     // // Process the weather data to ensure the summary is up-to-date
+//     // await processWeatherData({ city });
+
+//     // Fetch the summary for the current day
+//     const summary = await WeatherSummary.findOne({
+//       city,
+//       date: today,
+//     });
+
+//     if (!summary) {
+//       return res.status(404).json({ error: "No summary found for today" });
+//     }
+
+//     res.status(200).json(summary);
+//   } catch (error) {
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while fetching weather summary" });
+//   }
+// });
+
+router.get("/summaries/:city", async (req, res) => {
   try {
     const { city } = req.params;
-    const sevenDaysAgo = new Date();
-    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+    const summaries = await WeatherSummary.find({ city }).sort({ date: 1 });
 
-    await processWeatherData({ city });
+    if (!summaries || summaries.length === 0) {
+      return res.status(404).json({ error: "No summaries found for the city" });
+    }
 
-    const summary = await WeatherSummary.find({
-      city,
-      date: { $gte: sevenDaysAgo },
-    }).sort({ date: 1 });
-
-    res.status(200).json(summary);
+    res.status(200).json(summaries);
   } catch (error) {
     res
       .status(500)
-      .json({ error: "An error occurred while fetching weather summary" });
+      .json({ error: "An error occurred while fetching weather summaries" });
   }
 });
 
